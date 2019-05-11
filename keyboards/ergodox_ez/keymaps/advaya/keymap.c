b52fc8bb07cb4e830f816f3a02d37bae2a12ca36
Phredd Phlintstone <PhreddPhlintstone@gmail.com>
2019-05-10 22:34:06 -0700

    fixed capslock LED issue

--- keyboards/ergodox_ez/keymaps/advaya/keymap.c
+++ keyboards/ergodox_ez/keymaps/advaya/keymap.c
@@ -144,14 +144,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     case KC_CAPS: // Sw A
       if (record->event.pressed) {
-	if (rgb_matrix_config.val) {
+        if (host_keyboard_leds() & (1<<USB_LED_CAPS_LOCK)) {
           rgb_matrix_sethsv(rgb_matrix_config.hue, rgb_matrix_config.sat, 0);
+        } else {
+          rgb_matrix_sethsv(rgb_matrix_config.hue, rgb_matrix_config.sat, 255);
+        } 
+          // ergodox_right_led_3_on();
+      }
+      return true;
+     /* 	if (rgb_matrix_config.val) {
+            rgb_matrix_sethsv(rgb_matrix_config.hue, rgb_matrix_config.sat, 0);
         } else {
           rgb_matrix_sethsv(rgb_matrix_config.hue, rgb_matrix_config.sat, 255);
         }
        }
        return true;
-
+      */ 
         /* if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
 	  rgblight_mode(1); 
           rgblight_sethsv(24,255,255);
